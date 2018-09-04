befde5e00fa974ea2568516206c1196b4afe3b5c
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-03 20:19:17 -0600

    Adds unfavortite gif functionality

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,12 @@ class FavoritesController < ApplicationController
     redirect_to user_path(params[:user_id])
   end
   
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    redirect_to user_path(params[:user_id])
+  end
+  
   private 
     def favorite_params 
       params.require(:favorite).permit(:user_id, :gif_id)
