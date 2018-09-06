6adbf5017f28857d44b7d58d804f205190c5fba8
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-09-05 18:20:51 -0600

    Add sorting all gifs by category

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -2,6 +2,7 @@ class GifsController < ApplicationController
 
   def index 
     @catergories = Category.all
-    @gifs = Gif.all
+    @gifs = Gif.gifs_sorted_by_category
+    @gifs_slices = @gifs.all.each_slice(4)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @favorites = Favorite.where(user_id: params[:id])
+    find_favorites = Favorite.where(user_id: params[:id])
+    sort_favorites = find_favorites.favorites_sorted_by_category
+    @favorites_slices = sort_favorites.each_slice(4)
   end
   
   def new
